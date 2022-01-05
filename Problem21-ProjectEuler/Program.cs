a8f427e18c9b0c900c99cf01d563f98d40660278
nikil <nikil@DESKTOP-73MHGNO>
2022-01-05 11:18:22 -0500

    Add project files.

---
+++ Problem21-ProjectEuler/Program.cs
@@ -0,0 +1,29 @@
+﻿double totalSum = 0;
+
+for (int i = 2; i < 10000; i++)
+{
+    if (i == SumaFactores(SumaFactores(i)) && i != SumaFactores(i))
+    {
+        int test = SumaFactores(i);
+        int test2 = SumaFactores(SumaFactores(i));
+        totalSum = totalSum + i;
+    }
+}
+
+
+
+Console.WriteLine("The sum of factors is: " + totalSum);
+
+
+int SumaFactores(int value)
+{
+    int sum = 0;
+
+    for (int i = 1; i < value; i++)
+    {
+        if (value % i == 0)
+            sum = sum + i;
+    }
+
+    return sum;
+}
